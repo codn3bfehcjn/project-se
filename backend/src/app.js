bb13a20076853f70bd16fd35cd834b848374b222
vishwam thakur <vishwamthakur.cr7@gmail.com>
2025-11-04 21:58:48 +0530

    commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/check", (req, res) => res.send("Backend is running"));
+
 app.use("/api", aiRoutes);
 
 export default app;
